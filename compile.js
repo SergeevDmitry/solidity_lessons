6a6e8d55c31d654cae5aa1b38471a7c833ad12d5
sergeevdms <sergeevdms@gmail.com>
2022-02-13 01:36:46 +0300

    compile our first project

---
+++ compile.js
@@ -0,0 +1,10 @@
+// require('./contracts/Inbox.sol'); BAD!
+
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf-8');
+
+console.log(solc.compile(source, 1));
